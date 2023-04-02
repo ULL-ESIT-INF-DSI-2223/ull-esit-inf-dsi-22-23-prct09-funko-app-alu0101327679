de406c79f79bb9f94b85003ef35120ff7a844730
alu0101327679 <alu0101327679@ull.edu.es>
2023-04-02 23:29:34 +0000

    modificacion de laboratorio

---
+++ src/modificacion/RegistroJSON.ts
@@ -0,0 +1,26 @@
+import { Registro } from "./registro.js";
+import { Bolsa } from "./bolsa.js";
+import {readFile} from 'fs';
+
+
+/**
+ * clase registro de fichero JSON, no funciona
+ */
+export class RegistroJSON extends Registro {
+
+  constructor(Bolsa: Bolsa){
+    super(Bolsa)
+  }
+
+  protected pocesar(): void {
+    readFile('formato_JSON.json', (err, data) => {
+      if (err) {
+        console.log('There must be a problem with the file you are trying to read');
+      } else {
+        this.datos = data.toString()  
+        this.bolsa = JSON.parse(this.datos)
+        // console.log(this.bolsa)
+      }
+    });
+  }
+}
